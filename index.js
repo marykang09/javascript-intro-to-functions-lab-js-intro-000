ddf9d1de61d24c584a387b6983d0d858057d46f4
Mary Kang <mary.kang09@gmail.com>
2020-02-10 02:42:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,13 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string) {
-
-
+if (string.toLowerCase() === lowercase) {
+  return "I can't hear you!";
+}
+if (string.toUpperCase() === uppercase) {
+  return "YES INDEED!";
+}
+if (string === "I love you, Grandma.") {
+  return "I love you, too.";
+}
 }
